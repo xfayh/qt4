f00c02eab27b6a9dbed17e048a3e14dd61747393
xfayh <xfayh@163.com>
2018-12-16 00:23:41 +0800

    增加 html语法对输出的字符

--- qt-book/chap01/hello/hello.cpp
+++ qt-book/chap01/hello/hello.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QLabel *label = new QLabel("Hello Qt!");
+    QLabel *label = new QLabel("<h2><i>Hello</i> <font color=red>Qt!</font></h2>");
     label->show();
     return app.exec();
 }
